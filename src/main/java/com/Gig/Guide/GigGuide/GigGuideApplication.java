bc62e4c51082628672dcd46b10e88bb8cc46b6c1
Thizwilondi <gilbert.tweezytshamano@gmail.com>
2025-10-31 21:12:22 +0200

    Initial backend setup

---
+++ src/main/java/com/Gig/Guide/GigGuide/GigGuideApplication.java
@@ -0,0 +1,13 @@
+package com.Gig.Guide.GigGuide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GigGuideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GigGuideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Gig/Guide/GigGuide/GigGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Gig.Guide.GigGuide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GigGuideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
