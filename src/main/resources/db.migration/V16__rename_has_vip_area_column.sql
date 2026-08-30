8f91122ac64c0b99275f2e3334c0ab9316a528ad
Thizwilondi <gilbert.tweezytshamano@gmail.com>
2026-08-30 19:43:09 +0200

    fix: add V16 migration to rename has_v_i_p_area to hasviparea

--- src/main/resources/db.migration/V16__rename_has_vip_area_column.sql
+++ src/main/resources/db.migration/V16__rename_has_vip_area_column.sql
@@ -1,5 +1,2 @@
--- V16: Rename has_v_i_p_area to hasviparea to match the Hibernate entity mapping
--- The Clubs entity uses @Column(name = "hasviparea") which Hibernate resolves to "hasviparea"
--- but V4 created the column as "has_v_i_p_area"
-
+-- V16: Rename has_v_i_p_area to hasviparea to match the @Column(name = "hasviparea") in Clubs.java
 ALTER TABLE clubs RENAME COLUMN has_v_i_p_area TO hasviparea;
